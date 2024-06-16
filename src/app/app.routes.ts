fea1ed359fd587d9fc720ef2eb9d4ba005f08254
lsalvatierra <luisangel.salvat@gmail.com>
2024-06-15 21:43:20 -0500

    guard canactivate

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,11 +7,13 @@ import { CursoListaComponent } from './dashboard/cursos/curso-lista/curso-lista.
 import { EstadoListaComponent } from './dashboard/estados/estado-lista/estado-lista.component';
 import { PostListaComponent } from './dashboard/posts/post-lista/post-lista.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     /*{path: "amaterial", component: EjemploAmaterialComponent}*/
     {path: "login", component: LoginComponent},
     {path: "dashboard", component: DashboardComponent,
+        canActivate: [authGuard],
         children: [
             {path: "home", component: HomeComponent},
             {path: "curso", component: CursoListaComponent},            
