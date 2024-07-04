6a4daa938065d4794e3723fd0c0374d6387de8b1
gustavoqooo <luisangel.salvat@gmail.com>
2024-07-04 06:09:11 -0500

    toke api rest

--- src/app/dashboard/posts/post.service.ts
+++ src/app/dashboard/posts/post.service.ts
@@ -1,7 +1,11 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Post } from './post';
+
+const httphead = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +14,37 @@ export class PostService {
 
   constructor(private httpClient: HttpClient) { }
 
-  getAllPosts():Observable<Post[]>{
+  //Guardar valor en localstorage: localStorage.setItem('token', token)
+
+  private obtenerToken():string{
+    return localStorage.getItem('token') || ''
+  }
+
+  private cabeceraMensaje():HttpHeaders{
+    return new HttpHeaders({
+      'Authorization': `Bearer ${this.obtenerToken()}`,
+      'Content-Type': 'application/json'
+    })
+  }
+
+
+  
+
+  getAllPosts():Observable<Post[]>{    
     return this.httpClient.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
   }
+
+  getAllPostsToken():Observable<any>{
+    const headers = this.cabeceraMensaje();
+    return this.httpClient.get("https://jsonplaceholder.typicode.com/posts", {headers});
+  }
+
 
   getPostById(id: number): Observable<Post>{
     return this.httpClient.get<Post>("https://jsonplaceholder.typicode.com/posts/"+id);
   }
 
-  createPost(post: Post):Observable<Post>{
+  createPost(post: Post):Observable<Post>{    
     return this.httpClient.post<Post>("https://jsonplaceholder.typicode.com/posts", post);    
   }
 
